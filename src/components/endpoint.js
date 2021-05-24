9b310903375e4f5631beb5d6a5e01aee0f131cae
prabhusiva92 <76937366+prabhusiva92@users.noreply.github.com>
2021-05-24 03:40:34 -0800

    Add files via upload

---
+++ src/components/endpoint.js
@@ -0,0 +1,6 @@
+// const FRONTEND_ENDPOINT = 'http://localhost:3001';
+const FRONTEND_ENDPOINT = 'https://oven-starvy-pizza.netlify.app';
+// const BACKEND_ENDPOINT = 'http://localhost:3000';
+const BACKEND_ENDPOINT = 'https://oven-starvy-pizza-backend.herokuapp.com';
+
+module.exports = { FRONTEND_ENDPOINT, BACKEND_ENDPOINT };
